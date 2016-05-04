3f874fd547f2ca5504c4c07d9282cf604040158f
PatrycjaBanasiak <patrycja.ban@gmail.com>
2016-05-04 20:43:27 +0200

    error queue doesn`t work

--- src/main/java/camel/MainLogger.java
+++ src/main/java/camel/MainLogger.java
@@ -8,10 +8,11 @@ import org.slf4j.LoggerFactory;
  * Created by pizmak on 2016-04-25.
  */
 public class MainLogger {
-    private static final Logger logger = LoggerFactory.getLogger(MainLogger.class);
+    private static final Logger logger = LoggerFactory.getLogger("camelLogger");
 
     @Handler
     public void logMessage(String message){
-        logger.info(message);
+        System.out.println("---------------------------------------------------------MainLogger"+message);
+        logger.info("Camel info"+message);
     }
 }
